de4135a80192324af938a43fd15a052e70223eb8
Yamada Anna <66070734+YamadaYuu@users.noreply.github.com>
2020-09-15 19:11:26 -0300

    CadastroMotorista
    ok

---
+++ src/RoadTracker/src/main/java/view/ControlesTelaCadastroMotorista.java
@@ -0,0 +1,15 @@
+package view;
+
+
+	import javafx.fxml.FXML;
+	import javafx.scene.control.ComboBox;
+
+	public class ControlesTelaCadastroMotorista {
+
+	    @FXML
+	    private ComboBox<?> ComboBoxCargo;
+
+	    @FXML
+	    private ComboBox<?> ComboBoxTurno;
+
+	}
