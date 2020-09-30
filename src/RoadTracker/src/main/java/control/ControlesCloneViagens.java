881b4e3323b5af6347b2129b4117b101d1db52f0
Yamada Anna <66070734+YamadaYuu@users.noreply.github.com>
2020-09-30 16:42:10 -0300

    tela viagens
    interface

---
+++ src/RoadTracker/src/main/java/control/ControlesCloneViagens.java
@@ -0,0 +1,64 @@
+package control;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+
+public class ControlesCloneViagens {
+
+	    @FXML
+	    private TextField viagemInit;
+
+	    @FXML
+	    private TextField empresaDestino;
+
+	    @FXML
+	    private Button botaoBuscar;
+
+	    @FXML
+	    private Button botaoCadastraViagem;
+
+	    @FXML
+	    private TextField viagemFim;
+
+	    @FXML
+	    private Button botaoLimpaForm;
+
+	    @FXML
+	    private ComboBox<?> motorista;
+
+	    @FXML
+	    private ComboBox<?> idRastreador;
+
+	    @FXML
+	    void abrirTelaCadFunc(MouseEvent event) {
+
+	    }
+
+	    @FXML
+	    void abrirTelaFunc(MouseEvent event) {
+
+	    }
+
+	    @FXML
+	    void fecharJanela(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void minimizarJanela(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void voltar(ActionEvent event) {
+
+	    }
+
+}
+
